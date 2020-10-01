4a35e965fd5c2114cf226810339a438c0736324d
Avash027 <avashmitra2706@gmail.com>
2020-09-30 20:23:55 -0700

    Minor changes in UI

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,10 @@ const form = document.querySelector('#codef')
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
+
+    var codefbtn = document.querySelector('#codefbtn');
+    codefbtn.className = 'button is-link is-loading'
+
     const fetchurl = '/query?site=codeforces';
     
     fetch(fetchurl)
@@ -23,6 +27,7 @@ form.addEventListener('submit' , (e)=>{
                     }
                 })
                 cftable.innerHTML = tempdata+'</table><br>';
+                codefbtn.className='button is-link'
         } 
         })
     })
@@ -32,6 +37,10 @@ const formc = document.querySelector('#codec')
 
 formc.addEventListener('submit' , (e)=>{
     e.preventDefault();
+
+    var codecbtn = document.querySelector('#codecbtn');
+    codecbtn.className = 'button is-warning is-loading'
+
     const fetchurl = '/query?site=codechef';
 
     fetch(fetchurl)
@@ -46,6 +55,7 @@ formc.addEventListener('submit' , (e)=>{
                 data.data = data.data.replace(/<a/g , '<ab')
                 const d = `<p class="title is-1">CodeChef</p><a href="https://www.codechef.com/" target="_blank" >Checkout Codechef</a><br><table class="table is-striped is-hoverable is-fullwidth"><tr><td>ID</td><td>Name</td><td>START</td><td>END</td></tr>${data.data}</table>`
                 cctable.innerHTML = d;
+                codecbtn.className = 'button is-warning'
             }
         })
     })
